872bb3b1c1f272daaa6bfeeb3b352a9c21f35044
Martin Feckie <martin.feckie@3plearning.com>
2023-06-24 12:07:36 +0800

    Initial commit

---
+++ lib/ex_brotli.ex
@@ -0,0 +1,18 @@
+defmodule ExBrotli do
+  @moduledoc """
+  Documentation for `ExBrotli`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExBrotli.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_brotli_test.exs
@@ -0,0 +1,8 @@
+defmodule ExBrotliTest do
+  use ExUnit.Case
+  doctest ExBrotli
+
+  test "greets the world" do
+    assert ExBrotli.hello() == :world
+  end
+end
