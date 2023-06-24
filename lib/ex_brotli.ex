60ff7ae939498d8476775480a82ddb7ce3c9f25a
Martin Feckie <martin.feckie@3plearning.com>
2023-06-24 12:13:32 +0800

    Remove generator cruft

--- lib/ex_brotli.ex
+++ lib/ex_brotli.ex
@@ -1,18 +1,2 @@
 defmodule ExBrotli do
-  @moduledoc """
-  Documentation for `ExBrotli`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExBrotli.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/ex_brotli_test.exs
+++ test/ex_brotli_test.exs
@@ -1,8 +1,4 @@
 defmodule ExBrotliTest do
   use ExUnit.Case
   doctest ExBrotli
-
-  test "greets the world" do
-    assert ExBrotli.hello() == :world
-  end
 end
