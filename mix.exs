3bca6b1a5fb6ddd27d22bd976e69cadc4fcca12a
Martin Feckie <martin.feckie@3plearning.com>
2023-06-24 12:19:10 +0800

    Add version to mix

--- mix.exs
+++ mix.exs
@@ -1,10 +1,11 @@
 defmodule ExBrotli.MixProject do
   use Mix.Project
+  @version "0.1.0"
 
   def project do
     [
       app: :ex_brotli,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
