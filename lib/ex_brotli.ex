935a0a29925c348eaf034ba7c5517a591d9490f4
Martin Feckie <martin.feckie@3plearning.com>
2023-06-24 14:15:32 +0800

    Add docs

--- lib/ex_brotli.ex
+++ lib/ex_brotli.ex
@@ -2,6 +2,10 @@ defmodule ExBrotli do
   mix_config = Mix.Project.config()
   version = mix_config[:version]
 
+  @moduledoc """
+  Wrapper functions for interacting with the Rust based brotli library
+  """
+
   use RustlerPrecompiled,
     otp_app: :ex_brotli,
     crate: :exbrotli,
@@ -20,8 +24,21 @@ defmodule ExBrotli do
     ),
     version: version
 
+  @doc """
+  Compress a binary using brotli
+
+  ## Examples
+
+      iex> ExBrotli.compress("hello")
+      {:ok,  <<11, 2, 128, 104, 101, 108, 108, 111, 3>>}
+  """
   def compress(_binary), do: :erlang.nif_error(:nif_not_loaded)
 
+  @doc """
+  Compress a binary using brotli, raising an error if the compression fails
+
+  see `compress/1` for examples
+  """
   def compress!(binary) do
     case compress(binary) do
       {:ok, result} -> result
