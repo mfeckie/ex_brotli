1e4ee41d7b0b8688c2db8644797ae8466c7ca9a7
Martin Feckie <martin.feckie@3plearning.com>
2023-11-30 21:48:48 +0800

    Add nif call

--- lib/ex_brotli.ex
+++ lib/ex_brotli.ex
@@ -35,12 +35,34 @@ defmodule ExBrotli do
   def compress(_binary), do: :erlang.nif_error(:nif_not_loaded)
 
   @doc """
+  Decompress a binary using brotli
+
+  ## Examples
+
+      iex> ExBrotli.decompress(<<11, 2, 128, 104, 101, 108, 108, 111, 3>>)
+      {:ok, "hello"}
+  """
+  def decompress(_binary), do: :erlang.nif_error(:nif_not_loaded)
+
+  @doc """
   Compress a binary using brotli, raising an error if the compression fails
 
   see `compress/1` for examples
   """
   def compress!(binary) do
     case compress(binary) do
+      {:ok, result} -> result
+      {:error, reason} -> raise reason
+    end
+  end
+
+  @doc """
+  Decompress a binary using brotli, raising an error if the decompression fails
+
+  see `decompress/1` for examples
+  """
+  def decompress!(binary) do
+    case decompress(binary) do
       {:ok, result} -> result
       {:error, reason} -> raise reason
     end
